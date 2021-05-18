6cc0ad8a37e61e22d943fbe7204c249dcb51d507
Gabriel Bezerra <gabriel.1494@gmail.com>
2021-05-18 11:21:24 -0300

    Adicionando as chamadas

--- main.cpp
+++ main.cpp
@@ -85,50 +85,76 @@ int main(){
 
    if (aluno == 1 )
    {
-    Funcao f1(func);
-
+      Funcao f1(func);
+      if(f1.get_tipo_Polinomio()){
+        f1.get_dominio();
+        f1.get_imagem();
+        f1.get_raizes();
+        f1.plot(); 
+        double F1_x1 = f1.retorna_valor(p1);
+        double F1_x2 = f1.retorna_valor(p2);
+        double F1_x3 = f1.retorna_valor(p3);
+      }
+      else
+      {
+        f1.plot(); 
+        double F1_x1 = f1.retorna_valor(p1);
+        double F1_x2 = f1.retorna_valor(p2);
+        double F1_x3 = f1.retorna_valor(p3);
+      }
    } 
+
+   if (aluno == 2 )
+   {
+      Funcao f1(func);
+      if(f1.get_tipo_Polinomio()){
+
+        Calculo_Analitico f3(func);
+        f3.get_dominio();
+        f3.get_imagem();
+        f3.get_raizes(); 
+        f3.get_max_e_min();
+        f3.plot(); 
+
+        double   F3_x1  = f3.retorna_valor(p1);
+        double   F3_x2  = f3.retorna_valor(p2);
+        double   F3_x3  = f3.retorna_valor(p3);
+        double d_F3_x1  = f3.retorna_valor("derivada", d1);
+        double d_F3_x2  = f3.retorna_valor("derivada", d2);
+        double d_F3_x3  = f3.retorna_valor("derivada", d3);
+        double i_F3_ab1  = f3.retorna_valor("integral", li1, ls1);
+        double i_F3_ab2  = f3.retorna_valor("integral", li2, ls2);
+        double i_F3_ab3  = f3.retorna_valor("integral", li3, ls3);
+
+        Calculo_Numerico f2(func);
+
+        double d_F2_x1  = f2.retorna_valor("derivada", d1);
+        double d_F2_x2  = f2.retorna_valor("derivada", d2);
+        double d_F2_x3  = f2.retorna_valor("derivada", d3);
+        double i_F2_ab1  = f2.retorna_valor("integral", li1, ls1);
+        double i_F2_ab2  = f2.retorna_valor("integral", li2, ls2);
+        double i_F2_ab3  = f2.retorna_valor("integral", li3, ls3);
+
+
+      }
+      else
+      {
+        Calculo_Numerico f2(func);
+
+        f2.plot();
+        double   F2_x1  = f2.retorna_valor(p1);
+        double   F2_x2  = f2.retorna_valor(p2);
+        double   F2_x3  = f2.retorna_valor(p3);
+        double d_F2_x1  = f2.retorna_valor("derivada", d1);
+        double d_F2_x2  = f2.retorna_valor("derivada", d2);
+        double d_F2_x3  = f2.retorna_valor("derivada", d3);
+        double i_F2_ab1  = f2.retorna_valor("integral", li1, ls1);
+        double i_F2_ab2  = f2.retorna_valor("integral", li2, ls2);
+        double i_F2_ab3  = f2.retorna_valor("integral", li3, ls3);
+      }
+   } 
+
 
    return 0;
 }
 
-
-/*
-   // Todos os métodos que podemos usar em um objeto da classe Funcao.
-   Funcao f1("4 x^2 - 3 x");
-   f1.get_dominio();
-   f1.get_imagem();
-   f1.get_raizes();
-   f1.plot(); 
-
-   double F1_x0 = f1.retorna_valor(5.5);
-
-
-   // Todos os métodos que podemos usar em um objeto da classe Calculo_Numerico.
-   Calculo_Numerico f2("-2 -1 x -6x^2 -12x^3 +4x^5");
-   f2.get_dominio();
-   f2.get_imagem();
-   f2.get_raizes(); 
-   f2.get_max_e_min(-100., 100);
-   f2.plot(); 
-
-   double   F2_x0  = f2.retorna_valor(11.);
-   double d_F2_x0  = f2.retorna_valor("derivada", 13.1);
-   double i_F2_ab  = f2.retorna_valor("integral", 0., 15.);
-
-
-   // Todos os métodos que podemos usar em um objeto da classe Calculo_Analitico.
-   Calculo_Analitico f3("24-14x-13x^2+2x^3+x^4");
-   f3.get_dominio();
-   f3.get_imagem();
-   f3.get_raizes(); 
-   f3.get_max_e_min();
-   f3.plot(); 
-
-   double   F3_x0  = f3.retorna_valor(-5.);
-   double d_F3_x0  = f3.retorna_valor("derivada", 13.1);
-   double i_F3_x0  = f3.retorna_valor("integral", 5.);
-   double i_F3_ab  = f3.retorna_valor("integral", -5., 5.);
-
-*/
- 
