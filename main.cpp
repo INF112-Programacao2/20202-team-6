2543683b09e6b03c6ac282ffc23331ca85692eb8
Gabriel Bezerra <gabriel.1494@gmail.com>
2021-05-19 19:57:29 -0300

    Criando funcao pra leitura

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
- 
 
 #include "Funcao.h"
 #include "Grafico.h"
 #include "Calculo_Numerico.h"
 #include "Calculo_Analitico.h"
 
+std::string ler_input(int &aluno, double &p1, double &p2, double &p3, double &d1, double &d2, double &d3, double &li1, double &ls1, double &li2, double &ls2, double &li3, double &ls3);
+
 int main(){
+
+   std::string func;
+   int aluno;
+   double p1, p2, p3, d1, d2, d3, li1, ls1, li2, ls2, li3, ls3;
+
+    func = ler_input(aluno, p1, p2, p3, d1, d2, d3, li1, ls1, li2, ls2, li3, ls3);
    
     FILE* saida = fopen("RELATORIO.txt", "w");
     if(saida == NULL) {
@@ -17,86 +24,12 @@ int main(){
     }
     fclose(saida);
 
-   std::string leia, func;
-   int aluno;
-   double p1, p2, p3, d1, d2, d3, li1, ls1, li2, ls2, li3, ls3;
-   
-   std::ifstream entrada;
-   entrada.open("INPUT.txt");
-
-  if (entrada.is_open() && entrada.good())
-  { std::getline(entrada, leia);
-
-    while(!entrada.fail())
-     {
-        if (leia[0] != '#')
-        {
-                switch(leia[0]){
-                 case 'a' :
-                   entrada >> aluno;
-                   break;
-                 case 'f' :
-                   std::getline(entrada, func);
-                   break;
-                 case 'p' :
-                   {switch(leia[1]){
-                      case '1' :
-                       entrada >> p1;
-                       break;
-                      case '2' :
-                       entrada >> p2;
-                       break;
-                      case '3' :
-                       entrada >> p3;
-                       break;
-                     }                       
-                    }
-                   break;
-                 case 'd' :
-                   {switch(leia[1]){
-                      case '1' :
-                       entrada >> d1;
-                       break;
-                      case '2' :
-                       entrada >> d2;
-                       break;
-                      case '3' :
-                       entrada >> d3;
-                       break;
-                     }                      
-                    }
-                   break;
-                 case 'i' :
-                   {switch(leia[1]){
-                      case '1' :
-                       entrada >> li1 >> ls1;
-                       break;
-                      case '2' :
-                       entrada >> li2 >> ls2;
-                       break;
-                      case '3' :
-                       entrada >> li3 >> ls3;
-                       break;
-                     }
-                    }
-                   break;
-                }
-
-        }
-
-         std::getline(entrada, leia);        
-     }
-   }
-
-   entrada.close();
-
    if (aluno == 1 )
    {
       Funcao f1(func);
       if(f1.get_tipo_Polinomio()){
         f1.get_dominio();
         f1.get_imagem();
-        //f1.get_raizes();
         f1.plot(); 
         double F1_x1 = f1.retorna_valor(p1);
         double F1_x2 = f1.retorna_valor(p2);
@@ -135,8 +68,7 @@ int main(){
 
         Calculo_Analitico f3(func);
         f3.get_dominio();
-        f3.get_imagem();
-        //f3.get_raizes(); 
+        f3.get_imagem(); 
         f3.get_max_e_min();
         f3.plot(); 
 
@@ -221,4 +153,118 @@ int main(){
 
    return 0;
 }
+
+std::string ler_input(int &aluno, double &p1, double &p2, double &p3, double &d1, double &d2, double &d3, double &li1, double &ls1, double &li2, double &ls2, double &li3, double &ls3){
+
+   std::string leia, func;
+
+   std::ifstream entrada;
+   entrada.open("INPUT.txt");
+
+  if (entrada.is_open() && entrada.good())
+  { std::getline(entrada, leia);
+
+    while(!entrada.fail())
+     {
+        if (leia[0] != '#')
+        {
+                switch(leia[0]){
+                 case 'a' :
+                   entrada >> aluno;
+                   break;
+                 case 'f' :
+                   std::getline(entrada, func);
+                   break;
+                 case 'p' :
+                   {switch(leia[1]){
+                      case '1' :
+                       entrada >> p1;
+                       break;
+                      case '2' :
+                       entrada >> p2;
+                       break;
+                      case '3' :
+                       entrada >> p3;
+                       break;
+                     }                       
+                    }
+                   break;
+                 case 'd' :
+                   {switch(leia[1]){
+                      case '1' :
+                       entrada >> d1;
+                       break;
+                      case '2' :
+                       entrada >> d2;
+                       break;
+                      case '3' :
+                       entrada >> d3;
+                       break;
+                     }                      
+                    }
+                   break;
+                 case 'i' :
+                   {switch(leia[1]){
+                      case '1' :
+                       entrada >> li1 >> ls1;
+                       break;
+                      case '2' :
+                       entrada >> li2 >> ls2;
+                       break;
+                      case '3' :
+                       entrada >> li3 >> ls3;
+                       break;
+                     }
+                    }
+                   break;
+                }
+
+        }
+
+         std::getline(entrada, leia);        
+     }
+   }
+
+   entrada.close();
+
+ 
+   return func;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
